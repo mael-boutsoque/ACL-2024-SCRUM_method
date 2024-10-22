47be4cdda1d26aada4b69ea6dff767075272fb3b
theo59000 <guilmettheo@gmail.com>
2024-10-22 21:54:54 +0200

    Udpate Entities
    add map edges

--- jeu/src/main/java/fr/ul/acl/model/Entities.java
+++ jeu/src/main/java/fr/ul/acl/model/Entities.java
@@ -10,6 +10,11 @@ public class Entities {
         liste.add(new Map(0,0,1920*2,1080*2));
         liste.add(new Entity(0,0,100,100));
         liste.add(new Entity(300, 300,100,70));
+        //bord de map
+        liste.add(new Entity(0, -1,1920*2,1));//haut
+        liste.add(new Entity(0, 1080*2+1,1920*2,1));//bas
+        liste.add(new Entity(-1, 0,1,1080*2));//gauche
+        liste.add(new Entity(1920*2, 0,1,1080*2));//droite
         //liste.add(new MonstreTest(700,300,110,110));
         //liste.add(new MonstreTest(500,0,20,20));
         liste.add(new Player( 1920/4 , 1080/4));
