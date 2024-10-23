9e303ec3bcc23d0b94538ab750de70ae565dfc3a
theo59000 <guilmettheo@gmail.com>
2024-10-23 17:33:42 +0200

    image invisible
    Invisble is now transparent

--- jeu/src/main/java/fr/ul/acl/model/Invisible.java
+++ jeu/src/main/java/fr/ul/acl/model/Invisible.java
@@ -4,7 +4,7 @@ public class Invisible extends Entity {
     Invisible(int x,int y,int width,int height){
         super(x,y,width,height);
         this.is_colidable = true;
-        //image_path = "images\\map_3.png";
-        //load_image();
+        image_path = null;
+        load_image();
     }
 }
