f7126f8e4f0159273a3fb361824006f28a03bc9e
mael-boutsoque <mael.boutsoque@gmail.com>
2024-11-04 15:03:22 +0100

    Update Entities.java

--- jeu/src/main/java/fr/ul/acl/model/Entities.java
+++ jeu/src/main/java/fr/ul/acl/model/Entities.java
@@ -11,64 +11,65 @@ public class Entities {
      * the map is always the first entity
      */
     public Entities(int win_width,int win_height){
+        final int coef_carte = 6;
         liste = new ArrayList<Entity>() ;
-        liste.add(new Map(0,0,1200*4,630*4));
+        liste.add(new Map(0,0,1200*coef_carte,630*coef_carte));
         liste.add(new Entity(0,0,100,100));
         liste.add(new Entity(300, 300,100,70));
 
         //bord de map
-        liste.add(new Invisible(0, -10,1200*4,10));//haut
-        liste.add(new Invisible(0, 630*4,1200*4,0));//bas
-        liste.add(new Invisible(-10, 0,10,630*4));//gauche
-        liste.add(new Invisible(1200*4, 0,10,630*4));//droite
+        liste.add(new Invisible(0, -10,1200*coef_carte,10));//haut
+        liste.add(new Invisible(0, 630*coef_carte,1200*coef_carte,0));//bas
+        liste.add(new Invisible(-10, 0,10,630*coef_carte));//gauche
+        liste.add(new Invisible(1200*coef_carte, 0,10,630*coef_carte));//droite
 
         //Monstre
         liste.add(new MonstreTest(700,300,110,110));
 
         //Obstacle 
         //Bloc Gris
-        liste.add(new Invisible(110*4,25*4,45*4,144*4));
-        liste.add(new Invisible(206*4,25*4,70*4,96*4));
-        liste.add(new Invisible(374*4,337*4,70*4,120*4));
+        liste.add(new Invisible(110*coef_carte,25*coef_carte,45*coef_carte,144*coef_carte));
+        liste.add(new Invisible(206*coef_carte,25*coef_carte,70*coef_carte,96*coef_carte));
+        liste.add(new Invisible(374*coef_carte,337*coef_carte,70*coef_carte,120*coef_carte));
         //Lac bleu
-        liste.add(new Invisible(88*4,373*4,88*4,127*4));
-        liste.add(new Invisible(71*4,400*4,126*4,72*4));
+        liste.add(new Invisible(88*coef_carte,373*coef_carte,88*coef_carte,127*coef_carte));
+        liste.add(new Invisible(71*coef_carte,400*coef_carte,126*coef_carte,72*coef_carte));
         //Bloc gris avec escalier
-        liste.add(new Invisible(158*4,169*4,1*4,144*4));
-        liste.add(new Invisible(158*4,169*4,261*4,1*4));
-        liste.add(new Invisible(419*4,169*4,1*4,96*4));
-        liste.add(new Invisible(161*4,286*4,21*4,27*4));
-        liste.add(new Invisible(185*4,310*4,45*4,27*4));
-        liste.add(new Invisible(251*4,310*4,72*4,27*4));
-        liste.add(new Invisible(323*4,286*4,48*4,27*4));
-        liste.add(new Invisible(371*4,262*4,24*4,27*4));
-        liste.add(new Invisible(395*4,238*4,24*4,27*4));
+        liste.add(new Invisible(158*coef_carte,169*coef_carte,1*coef_carte,144*coef_carte));
+        liste.add(new Invisible(158*coef_carte,169*coef_carte,261*coef_carte,1*coef_carte));
+        liste.add(new Invisible(419*coef_carte,169*coef_carte,1*coef_carte,96*coef_carte));
+        liste.add(new Invisible(161*coef_carte,286*coef_carte,21*coef_carte,27*coef_carte));
+        liste.add(new Invisible(185*coef_carte,310*coef_carte,45*coef_carte,27*coef_carte));
+        liste.add(new Invisible(251*coef_carte,310*coef_carte,72*coef_carte,27*coef_carte));
+        liste.add(new Invisible(323*coef_carte,286*coef_carte,48*coef_carte,27*coef_carte));
+        liste.add(new Invisible(371*coef_carte,262*coef_carte,24*coef_carte,27*coef_carte));
+        liste.add(new Invisible(395*coef_carte,238*coef_carte,24*coef_carte,27*coef_carte));
         //Bloc gris 2 étages
-        liste.add(new Invisible(737*4,310*4,69*4,27*4));
-        liste.add(new Invisible(827*4,310*4,312*4,27*4));
-        liste.add(new Invisible(734*4,121*4,1*4,213*4));
-        liste.add(new Invisible(734*4,121*4,22*4,1*4));
-        liste.add(new Invisible(758*4,96*4,1*4,94*4));
-        liste.add(new Invisible(758*4,193*4,50*4,21*4));
-        liste.add(new Invisible(806*4,193*4,1*4,40*4));
-        liste.add(new Invisible(806*4,236*4,72*4,26*4));
-        liste.add(new Invisible(899*4,238*4,21*4,24*4));
-        liste.add(new Invisible(758*4,73*4,70*4,24*4));
-        liste.add(new Invisible(828*4,97*4,95*4,1*4));
-        liste.add(new Invisible(923*4,25*4,216*4,240*4));
-        liste.add(new Invisible(1139*4,265*4,1*4,42*4));
+        liste.add(new Invisible(737*coef_carte,310*coef_carte,69*coef_carte,27*coef_carte));
+        liste.add(new Invisible(827*coef_carte,310*coef_carte,312*coef_carte,27*coef_carte));
+        liste.add(new Invisible(734*coef_carte,121*coef_carte,1*coef_carte,213*coef_carte));
+        liste.add(new Invisible(734*coef_carte,121*coef_carte,22*coef_carte,1*coef_carte));
+        liste.add(new Invisible(758*coef_carte,96*coef_carte,1*coef_carte,94*coef_carte));
+        liste.add(new Invisible(758*coef_carte,193*coef_carte,50*coef_carte,21*coef_carte));
+        liste.add(new Invisible(806*coef_carte,193*coef_carte,1*coef_carte,40*coef_carte));
+        liste.add(new Invisible(806*coef_carte,236*coef_carte,72*coef_carte,26*coef_carte));
+        liste.add(new Invisible(899*coef_carte,238*coef_carte,21*coef_carte,24*coef_carte));
+        liste.add(new Invisible(758*coef_carte,73*coef_carte,70*coef_carte,24*coef_carte));
+        liste.add(new Invisible(828*coef_carte,97*coef_carte,95*coef_carte,1*coef_carte));
+        liste.add(new Invisible(923*coef_carte,25*coef_carte,216*coef_carte,240*coef_carte));
+        liste.add(new Invisible(1139*coef_carte,265*coef_carte,1*coef_carte,42*coef_carte));
         //Gazon
-        liste.add(new Invisible(151*4,483*4,61*4,51*4));
-        liste.add(new Invisible(200*4,385*4,51*4,102*4));
+        liste.add(new Invisible(151*coef_carte,483*coef_carte,61*coef_carte,51*coef_carte));
+        liste.add(new Invisible(200*coef_carte,385*coef_carte,51*coef_carte,102*coef_carte));
         //Cactus
-        liste.add(new Invisible(350*4,28*4,45*4,42*4));
-        liste.add(new Invisible(182*4,556*4,39*4,38*4));
-        liste.add(new Invisible(254*4,460*4,45*4,43*4));
-        liste.add(new Invisible(758*4,532*4,42*4,42*4));
+        liste.add(new Invisible(350*coef_carte,28*coef_carte,45*coef_carte,42*coef_carte));
+        liste.add(new Invisible(182*coef_carte,556*coef_carte,39*coef_carte,38*coef_carte));
+        liste.add(new Invisible(254*coef_carte,460*coef_carte,45*coef_carte,43*coef_carte));
+        liste.add(new Invisible(758*coef_carte,532*coef_carte,42*coef_carte,42*coef_carte));
         //Rochers
-        liste.add(new Invisible(806*4,389*4,39*4,41*4));
-        liste.add(new Invisible(280*4,550*4,40*4,23*4));
-        liste.add(new Invisible(1022*4,478*4,40*4,23*4));
+        liste.add(new Invisible(806*coef_carte,389*coef_carte,39*coef_carte,41*coef_carte));
+        liste.add(new Invisible(280*coef_carte,550*coef_carte,40*coef_carte,23*coef_carte));
+        liste.add(new Invisible(1022*coef_carte,478*coef_carte,40*coef_carte,23*coef_carte));
 
         //liste.add(new MonstreTest(700,300,110,110));
         //liste.add(new MonstreTest(500,0,20,20));
