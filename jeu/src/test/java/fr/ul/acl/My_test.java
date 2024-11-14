0c7169005ff680263c229716531982f4ff450881
mael-boutsoque <mael.boutsoque@gmail.com>
2024-11-14 17:03:17 +0100

    ajout test collission

--- jeu/src/test/java/fr/ul/acl/My_test.java
+++ jeu/src/test/java/fr/ul/acl/My_test.java
@@ -20,4 +20,25 @@ public class My_test {
         Boolean test_mis_en_place = true;
         Assert.assertTrue(test_mis_en_place);
     }
+
+    @Test
+    public void Test_collision(){
+        Hitbox box1 = new Hitbox(0,0,100,100);
+        Hitbox box2 = new Hitbox(99,99,100,100);
+        Assert.assertTrue(box1.colide(box2));
+    }
+
+    @Test
+    public void Test_no_collision(){
+        Hitbox box1 = new Hitbox(0,0,100,100);
+        Hitbox box2 = new Hitbox(100,100,100,100);
+        Assert.assertFalse(box1.colide(box2));
+    }
+
+    @Test
+    public void Test_collision_zeros(){
+        Hitbox box1 = new Hitbox(0,0,0,0);
+        Hitbox box2 = new Hitbox(0,0,10,10);
+        Assert.assertFalse(box1.colide(box2));
+    }
 }
